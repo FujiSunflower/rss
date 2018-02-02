afd0b8e377e71add169f493b331d10fc8efbd24b
rn9dfj3 <hazcyqund84@yahoo.co.jp>
2018-02-03 07:22:37 +0900

    [Upgrade] Test browsing
    Browsing about URL was tested

--- rss/rss/rss.py
+++ rss/rss/rss.py
@@ -3,4 +3,11 @@ Created on 2018/02/03
 
 @author: rn9dfj3
 '''
-print("Hello")
+#print("Hello")
+#import bpy
+import webbrowser
+url = "https://github.com/rn9dfj3/rss"
+#webbrowser.get('mozilla')
+#print( webbrowser.browser )
+#webbrowser.get('firefox')
+webbrowser.open(url)
